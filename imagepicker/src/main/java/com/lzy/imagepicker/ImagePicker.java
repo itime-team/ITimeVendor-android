c40be9292cc700b127eb21d108c90b30e58b7740
HuangQiushuo <379258670@qq.com>
2017-02-15 16:06:11 +1100

    modify: camera

--- imagepicker/src/main/java/com/lzy/imagepicker/ImagePicker.java
+++ imagepicker/src/main/java/com/lzy/imagepicker/ImagePicker.java
@@ -243,19 +243,18 @@ public class ImagePicker {
     /** 拍照的方法 */
     public void takePicture(Activity activity, int requestCode) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        takePictureIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        takePictureIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         if (takePictureIntent.resolveActivity(activity.getPackageManager()) != null) {
             if (Utils.existSDCard())
                 takeImageFile = new File(Environment.getExternalStorageDirectory(), "/DCIM/camera/");
             else takeImageFile = Environment.getDataDirectory();
             takeImageFile = createFile(takeImageFile, "IMG_", ".jpg");
-            if (takeImageFile != null) {
+
                 // 默认情况下，即不需要指定intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
                 // 照相机有自己默认的存储路径，拍摄的照片将返回一个缩略图。如果想访问原始图片，
                 // 可以通过dat extra能够得到原始图片位置。即，如果指定了目标uri，data就没有数据，
                 // 如果没有指定uri，则data就返回有数据！
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(takeImageFile));
-            }
         }
         activity.startActivityForResult(takePictureIntent, requestCode);
     }
